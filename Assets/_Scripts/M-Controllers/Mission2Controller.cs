8ce40e8933d1802b70ac1d0e2bf74507fee55124
Tuhin Mridha <tuhinmridha11@gmail.com>
2021-12-26 02:59:04 +0600

    Finalised Mission 2 Cutscene

--- Assets/_Scripts/M-Controllers/Mission2Controller.cs
+++ Assets/_Scripts/M-Controllers/Mission2Controller.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Mission2Controller : MonoBehaviour
 {
+    public GameObject player;
+
+    public GameObject police;
+
+    private Animator playerAnimator;
+
+    private Animator policeAnimator;
+
+    private PlayerController playerController;
+
+    private Police policeController;
+
+    public GameObject playerGun;
+
+    public GameObject policeGun;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = player.GetComponent<PlayerController>();
+        policeController = police.GetComponent<Police>();
+        playerAnimator = player.GetComponentInChildren<Animator>();
+        policeAnimator = police.GetComponentInChildren<Animator>();
+        StartCoroutine(lookToplayer());
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+    }
+
+    IEnumerator playerTurnOffAnimation()
+    {
+        yield return new WaitForSeconds(2.0f);
+        playerAnimator.SetTrigger("isIdle");
+    }
+    
+
+    IEnumerator policeTurnOffAnimation()
+    {
+        yield return new WaitForSeconds(7.0f);
+        policeAnimator.SetTrigger("isIdle");
+    }
+
+    IEnumerator swapGun()
+    {
+        StaticVars.showWaypointMarker = true;
+        yield return new WaitForSeconds(58f);
+        policeGun.SetActive(false);
+        playerGun.SetActive(true);
+        
+    }
+
+    IEnumerator lookToplayer()
+    {
+        yield return new WaitForSeconds(22f);
+        police.transform.LookAt(player.transform);
     }
 }
